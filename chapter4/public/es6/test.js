8fe786334c2bafbfa405176e21d144fee1e3981d
neretin-trike <hawktrike@gmail.com>
2018-04-05 22:23:22 +0700

    Решил задачку конвертации арабских чисел в римские с codecamp

--- chapter4/public/es6/test.js
+++ chapter4/public/es6/test.js
@@ -1,18 +1,37 @@
 "use strict";
 
-// средство es6: блок "левых" объявлений
-const sentences = [
-	{ subject: "JavaScript", verb: "is", object: "great" },
-	{ subject: "Elephants", verb: "are", object: "large" },
-];
+function convertToRoman(num) {
 
-// средство es6: деструктуризация объекта
-function say ( {subject, verb, object } ) {
-	// средство es6: строки шаблона
-	console.log(`${subject} ${verb} ${object}`);
-}
+	let map = [
+		['','I','II','III','IV','V','VI','VII','VIII','IX'],
+		['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
+		['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+		['','M','MM','MMM']
+	  ];
+	
+	  
+	let unit1 = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
+	let unit10 = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'];
+	let unit100 = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'];
+	let unit1000 = ['','M','MM','MMM'];
+	
+	num = num.toString().split('');
+	let number = '';
+	let i = num.length-1;
 
-// средство es6: for .. of
-for (let s of sentences) {
-	say (s);
+	for (let n of num) {
+		number += map[i][n];
+		i--
+	}
+
+	// switch (num.length) {
+	// 	case 1: { number = unit1[num[0]]; break};
+	// 	case 2: { number = unit10[num[0]] + unit1[num[1]]; break};
+	// 	case 3: { number = unit100[num[0]] + unit10[num[1]] + unit1[num[2]]; break};
+	// 	case 4: { number = unit1000[num[0]] + unit100[num[1]] + unit10[num[2]] + unit1[num[3]]; break};
+	// }
+	return console.log(number);
 }
+   
+convertToRoman(56);
+   
