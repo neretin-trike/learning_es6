2f3d24da72191bcef617e6b9aa4d48c2c9beeab5
neretin-trike <hawktrike@gmail.com>
2018-04-05 20:09:28 +0700

    Завершил создание программы Корона и Якорь

--- chapter4/es6/crownAndAnchor.js
+++ chapter4/es6/crownAndAnchor.js
@@ -1,17 +1,29 @@
 "use strict";
 
-let funds = 50;
+function rand(m,n) {
+	return m + Math.floor((n - m + 1)*Math.random());
+}
 
-const bets = {
-	crown: 0, 
-	anchor: 0,
-	heart: 0,
-	spade: 0,
-	club: 0,
-	diamond: 0 
-};
+function randFace() {
+	return ["crown", "anchor", "heart", "spade", "club", "diamond"] [rand (0,5)];
+}
+
+let funds = 50,
+	round = 0;
 
 while (funds > 1 && funds < 100) {
+	round++;
+	console.log(`round ${round}:`);
+	console.log(`\tstarting funds: ${funds}p`)
+	
+	const bets = {
+		crown: 0, 
+		anchor: 0,
+		heart: 0,
+		spade: 0,
+		club: 0,
+		diamond: 0 
+	};
 
 	let totalBet = rand(1, funds);
 
@@ -20,30 +32,37 @@ while (funds > 1 && funds < 100) {
 		bets.heart = totalBet;
 	}
 	else {
-		// 
+		let remaining = totalBet;	
+
+		do {
+			let bet = rand(1, remaining);
+			let face = randFace();
+
+			bets[face] = bets[face] + bet;
+			remaining = remaining - bet;
+
+		} while (remaining > 0);
 	}
 
 	funds = funds - totalBet;
-	
-	let remaining = totalBet;	
-	do {
-		let bet = rand(1, remaining);
-		let face = randFace();
-		bets[face] = bets[face] + bet;
-		remaining = remaining - bet;
-	} while (remaining > 0);
+
+	console.log(`\tbets: ` + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(',') + ` (total: ${totalBet} pence)`);
 
 	const hand = [];
-
 	for (let roll = 0; roll<3; roll++) {
 		hand.push(randFace());
 	}
-}
 
-function rand(m,n) {
-	return m + Math.floor((n - m + 1)*Math.random());
-}
+	console.log(`\thand: ${hand.join(', ')}`)
 
-function randFace() {
-	return ["crown", "anchor", "heart", "spade", "club", "diamond"] [rand (0,5)];
+	let winnings = 0;
+	for (let die = 0; die<hand.length; die++) {
+		let face = hand[die];
+
+		if (bets[face] > 0) winnings = winnings + bets[face];
+	}
+	funds = funds + winnings;
+
+	console.log(`\twinnings: ${winnings}`);
 }
+console.log(`\tending funds: ${funds}`);
