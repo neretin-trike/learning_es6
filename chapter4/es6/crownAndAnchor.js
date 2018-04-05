a3bb73645968465439fae6deff54f2d9b3516c8f
neretin-trike <hawktrike@gmail.com>
2018-04-05 18:25:18 +0700

    Добавил условие выпадения 7 монет

--- chapter4/es6/crownAndAnchor.js
+++ chapter4/es6/crownAndAnchor.js
@@ -2,9 +2,29 @@
 
 let funds = 50;
 
+const bets = {
+	crown: 0, 
+	anchor: 0,
+	heart: 0,
+	spade: 0,
+	club: 0,
+	diamond: 0 
+};
+
 while (fund > 1 && funds < 100) {
-	funds = funds + 2;
-	funds = funds - 1;
+
+	let totalBet = rand(1, funds);
+
+	if (totalBet === 7) {
+		totalBet = funds;
+		bets.heart = totalBet;
+	}
+	else {
+		// 
+	}
+
+	funds = funds - totalBet;
+	
 }
 
 function rand(m,n) {
